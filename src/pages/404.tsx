fb486168ff039b43a66b93d082053f5aaa2ec14a
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-10-29 02:51:10 +0200

    Add not found page

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+export default function NotFoundPage() {
+    return (
+        <section className="w-screen h-screen flex justify-center items-center">
+            <div className="text-center">
+                <h1 className="text-8xl">404</h1>
+                <p className="text-2xl">Página no encontrada</p>
+            </div>
+        </section>
+    )
+}
