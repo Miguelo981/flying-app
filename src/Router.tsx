99750a7c69c42e81225c41612ea6edcfbcd105fa
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-10-29 02:51:03 +0200

    Add router

---
+++ src/Router.tsx
@@ -0,0 +1,23 @@
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+} from 'react-router-dom'
+import HomePage from '@/pages/HomePage'
+import Header from '@/components/Layout/Header'
+import HotelPage from '@/pages/HotelPage'
+import SuccessPage from '@/pages/Success'
+import CheckoutPage from '@/pages/Checkout'
+import NotFoundPage from './pages/404'
+
+export const AppRouter = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Header />}>
+            <Route index path="/" element={<HomePage />} />
+            <Route path="/hotel/:hotelId" element={<HotelPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+        </Route>
+    )
+)
